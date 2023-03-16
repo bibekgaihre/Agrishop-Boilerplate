404c965c0822dd6037c71587ea55a2c32533a474
bibekgaihre <bibekgaihre123@outlook.com>
2023-03-16 10:21:36 +0100

    🎉 frontend setup
    frontend setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      green: colors.green,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
+      nightgreen: "#c3e6cb",
+      nightgreenlight: "#b1dfbb",
+      warning: "#ffc107",
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
